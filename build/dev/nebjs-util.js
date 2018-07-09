0f97a2766c1bec4e652ad5fc93336f5e4705693c
nebjs <40974765+nebjs@users.noreply.github.com>
2018-07-09 23:00:16 +0800

    common bug and rebuild
    common bug and rebuild

--- build/dev/nebjs-util.js
+++ build/dev/nebjs-util.js
@@ -101,55 +101,92 @@ return /******/ (function(modules) { // webpackBootstrap
   !*** ./lib/array/index.js ***!
   \****************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
+const { cloneValue } = __webpack_require__(/*! ../common */ "./lib/common.js");
 /**
- * 唯一压入
- * @param array
+ * 拷贝
+ * @param array {Array} 目标
+ * @param element {*|Array} 源对象
+ * @param option {Object} 配置
+ * index {Number}： 非负整数，指定插入的位置，未指定时在最后，为0时为首
+ * multi {Boolean}：批量复制，默认为false，当为true时，element为要导入的多个元素组成的数组
+ * unique {Boolean}：唯一性限制，默认为false，当为true时，已经存在的元素不会被拷贝
+ * filter {Function} 过滤器：Function(array, elements, element, index)，过滤掉不合条件的元素（优先级低于deep配置），过滤器的this指向当前源element
+ *    返回值是false或其他非真对象时代表过滤掉相应元素
+ *    返回值是true时代表直接插入相应元素
+ *    返回值是{value: ???}时代表插入value对应的值，此方式可用于作特殊处理，比如实现复杂深度拷贝副本再插入等操作
+ * @return []
  */
-const uniquePush = function (array) {
-  const args = Array.prototype.slice.call(arguments, 1);
-  for (const arg of args) {
-    const index = array.findIndex(elem => {
-      return elem === arg;
-    });
-    if (index === -1) {
-      array.push(arg);
+const copy = function (array, element, option = {}) {
+  if (!Array.isArray(array)) throw new TypeError('array must be a array');
+  if (!option || option.constructor !== Object) throw new TypeError('option must be a object');
+  const { index, multi, unique = false, filter, deep = false } = option;
+  let es;
+  if (index !== undefined && (typeof index !== 'number' || index < 0 || index % 1 !== 0)) throw new TypeError('option\'s index must be a non-negative integer');
+  if (multi) {
+    if (!Array.isArray(element)) throw new TypeError('when option\'s multi is true, then elem must be a array');
+    es = element;
+  } else {
+    es = [element];
+  }
+  if (unique !== true && unique !== false) throw new TypeError('option\'s unique must be a boolean');
+  if (filter && typeof filter !== 'function') throw new TypeError('option\'s filter must be a function');
+  if (deep !== true && deep !== false) throw new TypeError('option\'s deep must be a boolean');
+  const len = es.length;
+  if (len > 0) {
+    let hasSame;
+    if (unique) {
+      hasSame = arg => {
+        return array.findIndex(elem => {
+          return elem === arg;
+        }) !== -1;
+      };
+    }
+    for (let i = 0; i < len; ++i) {
+      const e = es[i];
+      if (hasSame && hasSame(e)) continue;
+      let val;
+      if (filter) {
+        const back = filter.call(e, array, es, e, i);
+        if (!back) continue;
+        if (back.constructor === Object) {
+          val = back.value;
+        } else {
+          val = e;
+        }
+      } else {
+        val = e;
+      }
+      if (deep) {
+        val = cloneValue(val);
+      }
+      // 压入
+      if (index !== undefined) {
+        array.splice(index + i, 0, val);
+      } else {
+        array.push(val);
+      }
     }
   }
   return array;
 };
 const util = {
-  uniquePush
+  copy
 };
 module.exports = util;
 
 /***/ }),
 
-/***/ "./lib/index.js":
-/*!**********************!*\
-  !*** ./lib/index.js ***!
-  \**********************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-const object = __webpack_require__(/*! ./object/index */ "./lib/object/index.js");
-const string = __webpack_require__(/*! ./string/index */ "./lib/string/index.js");
-const array = __webpack_require__(/*! ./array/index */ "./lib/array/index.js");
-const util = { object, string, array };
-module.exports = util;
-
-/***/ }),
-
-/***/ "./lib/object/index.js":
-/*!*****************************!*\
-  !*** ./lib/object/index.js ***!
-  \*****************************/
+/***/ "./lib/common.js":
+/*!***********************!*\
+  !*** ./lib/common.js ***!
+  \***********************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
 const hasOwnProperty = Object.prototype.hasOwnProperty;
-const isObj = function (x) {
+const isObject = function (x) {
   return x !== null && x !== undefined && typeof x === 'object';
 };
 const assignKey = function (to, from, fromName, toName = fromName, mergeObject, mergeArray) {
@@ -162,7 +199,7 @@ const assignKey = function (to, from, fromName, toName = fromName, mergeObject, 
           fromElem = from[fromName];
     let toElem = to[toName];
     if (fromElem !== toElem) {
-      if (!isObj(fromElem) /*|| !hasOwnProperty.call(from, fromName)*/) {
+      if (!isObject(fromElem) /*|| !hasOwnProperty.call(from, fromName)*/) {
           to[toName] = fromElem;
         } else if (Array.isArray(fromElem)) {
         const len = fromElem.length;
@@ -174,7 +211,7 @@ const assignKey = function (to, from, fromName, toName = fromName, mergeObject, 
           stack.push({ to: toElem, from: fromElem, fromName: i, toName: i + toLen });
         }
       } else {
-        if (!mergeObject || !isObj(toElem)) {
+        if (!mergeObject || !isObject(toElem)) {
           toElem = to[toName] = {};
         }
         for (const name in fromElem) {
@@ -186,14 +223,65 @@ const assignKey = function (to, from, fromName, toName = fromName, mergeObject, 
     }
   }
 };
+const cloneValue = function (val) {
+  if (val && !(val instanceof Date) && !(val instanceof Error) && !(val instanceof RegExp)) {
+    const tp = typeof val;
+    if (tp === 'string') {
+      val = val.slice();
+    } else if (tp !== 'function' && tp !== 'boolean' && tp !== 'number' && tp !== 'symbol') {
+      let to;
+      if (Array.isArray(val)) {
+        to = [];
+      } else if (val.constructor === Object) {
+        to = {};
+      } else {
+        to = new val.constructor();
+      }
+      for (const key in val) {
+        if (hasOwnProperty.call(val, key)) {
+          assignKey(to, val, key, key);
+        }
+      }
+      val = to;
+    }
+  }
+  return val;
+};
+const common = { hasOwnProperty, isObject, assignKey, cloneValue };
+module.exports = common;
+
+/***/ }),
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const common = __webpack_require__(/*! ./common */ "./lib/common.js");
+const object = __webpack_require__(/*! ./object/index */ "./lib/object/index.js");
+const string = __webpack_require__(/*! ./string/index */ "./lib/string/index.js");
+const array = __webpack_require__(/*! ./array/index */ "./lib/array/index.js");
+const util = { common, object, string, array };
+module.exports = util;
+
+/***/ }),
+
+/***/ "./lib/object/index.js":
+/*!*****************************!*\
+  !*** ./lib/object/index.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const { hasOwnProperty, assignKey } = __webpack_require__(/*! ../common */ "./lib/common.js");
 /**
  * 清空对象
  * @param obj
  */
 const clear = function (obj) {
-  if (obj === null || obj === undefined) {
-    throw new TypeError('target cannot be null or undefined');
-  }
+  if (!obj || obj.constructor !== Object) throw new TypeError('obj must be a object');
   obj = Object(obj);
   for (const key in obj) {
     if (hasOwnProperty.call(obj, key)) {
@@ -206,7 +294,7 @@ const clear = function (obj) {
  * 深拷贝一个对象至另一个对象，可指定只拷贝满足条件的属性，可为属性指定别名
  * @param to {Object} 目标
  * @param from {Object} 源对象
- * @param option 配置
+ * @param option {Object} 配置
  * omit {String|Array|Function} : 要删除的属性，不会再触发后面的过滤器
  * filter {Function} 过滤器：Function(key, src, target)，过滤掉不合条件的属性（优先级最低），过滤器的this指向源对象src
  *    返回值为false的被过滤掉..
@@ -216,6 +304,9 @@ const clear = function (obj) {
  * @return {*}
  */
 const copy = function (to, from, option = {}) {
+  if (!to || to.constructor !== Object) throw new TypeError('to must be a object');
+  if (!from || from.constructor !== Object) throw new TypeError('from must be a object');
+  if (!option || option.constructor !== Object) throw new TypeError('option must be a object');
   let { omit, filter, mergeObject = true, mergeArray = true } = option;
   if (omit) {
     if (typeof omit === 'string') {
@@ -223,7 +314,7 @@ const copy = function (to, from, option = {}) {
     }
     if (omit.length === 0) omit = null;
   }
-  if (filter && typeof filter !== 'function') filter = null;
+  if (filter && typeof filter !== 'function') throw new TypeError('option\'s filter must be a function');
   if (omit || filter) {
     for (const key in from) {
       if (hasOwnProperty.call(from, key)) {
@@ -259,7 +350,7 @@ const copy = function (to, from, option = {}) {
  * 深选择拷贝一个对象至另一个对象，必须指定要拷贝的属性，可指定拷贝条件，可为属性指定别名
  * @param to {Object} 目标
  * @param from {Object} 源对象
- * @param option 配置 {Object}
+ * @param option {Object} 配置
  * pick {String|Object|Array}: 要拷贝的属性，其他属性不会触发后续的过滤器
  *    当为Object时：{srcAttrName: 'toAttrName', ...}  【源对象属性名称】对应的值作为【目标对象属性名称】（更名拷贝）
  * filter {Function} 过滤器：{Function(key, src, target)}，过滤掉不合条件的属性（优先级最低），过滤器的this指向源对象src
@@ -270,6 +361,9 @@ const copy = function (to, from, option = {}) {
  * @return {*}
  */
 const pick = function (to, from, option = {}) {
+  if (!to || to.constructor !== Object) throw new TypeError('to must be a object');
+  if (!from || from.constructor !== Object) throw new TypeError('from must be a object');
+  if (!option || option.constructor !== Object) throw new TypeError('option must be a object');
   let { pick, filter, mergeObject = true, mergeArray = true } = option;
   if (to === from) {
     return to;
@@ -281,7 +375,7 @@ const pick = function (to, from, option = {}) {
     if (pick.length === 0) pick = null;
   }
   if (!pick) return to;
-  if (filter && typeof filter !== 'function') filter = null;
+  if (filter && typeof filter !== 'function') throw new TypeError('option\'s filter must be a function');
   for (const key of pick) {
     if (hasOwnProperty.call(from, key)) {
       if (filter) {
@@ -366,6 +460,7 @@ const util = {
    * @returns {*}
    */
   trim(str) {
+    if (typeof str !== 'string') return new TypeError('str must be a string');
     return str.replace(reg.trim, '');
   },
   /**
@@ -374,6 +469,7 @@ const util = {
    * @returns {*}
    */
   trimLeft(str) {
+    if (typeof str !== 'string') return new TypeError('str must be a string');
     return str.replace(reg.trimLeft, '');
   },
   /**
@@ -382,15 +478,17 @@ const util = {
    * @returns {*}
    */
   trimRight(str) {
+    if (typeof str !== 'string') return new TypeError('str must be a string');
     return str.replace(reg.trimRight, '');
   },
   /**
    * 对字符串中的转义字符进行转义
-   * @param code
+   * @param str
    * @returns {*}
    */
-  escape(code) {
-    return code.length > 0 ? code.replace(reg.escape, escapeReplaceFun) : code;
+  escape(str) {
+    if (typeof str !== 'string') return new TypeError('code must be a string');
+    return str.length > 0 ? str.replace(reg.escape, escapeReplaceFun) : str;
   }
 };
 module.exports = util;
